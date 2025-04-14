2a11d8301391e4ddec4b18a8d8fd06aaec82f1aa
grahacr <courtney.graham@atlasschool.com>
2025-04-14 14:04:01 -0500

    auth clean up in testimonials and about page, working locally

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,8 @@
-import { initializeApp } from 'firebase/app'
+import { initializeApp, getApps, getApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
+
+console.log('Firebase/config.ts loaded')
 
 export const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API,

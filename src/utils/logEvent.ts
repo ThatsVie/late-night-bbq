ccdad80414d9b0e2ea313c58bb010ff7210edafc
grahacr <courtney.graham@atlasschool.com>
2025-04-15 22:51:44 -0500

    fix typescript issue causing build fail

--- src/utils/logEvent.ts
+++ src/utils/logEvent.ts
@@ -1,4 +1,4 @@
-export async function logFirebaseEvent(eventName: string, eventParams?: Record<string, any>) {
+export async function logFirebaseEvent(eventName: string, eventParams?: Record<string, unknown>) {
     if (typeof window !== 'undefined') {
         const { getAnalytics, isSupported, logEvent } = await import ('firebase/analytics')
         const { app } = await import('@/firebase/config')
